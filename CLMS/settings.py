7cd6004a01bdc248a273ac94d7ece3a49ae9154d
kensidiangco <kensidiangco@gmail.com>
2022-10-11 23:45:06 +0800

    Automatic configuration with django-heroku

--- CLMS/settings.py
+++ CLMS/settings.py
@@ -9,9 +9,11 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
 import os
 from pathlib import Path
+
+import django_heroku
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
