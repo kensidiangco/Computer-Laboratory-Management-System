8c03a22596b44560574814d0de9a2808bd87dba1
kensidiangco <kensidiangco@gmail.com>
2022-10-11 23:26:10 +0800

    Configuring tailwind to deploy

---
+++ theme/management/commands/collectstatic.py
@@ -0,0 +1,12 @@
+# theme/management/commands/collectstatic.py
+
+from django.core.management import call_command
+from django.contrib.staticfiles.management.commands.collectstatic import (
+    Command as CollectStaticCommand,
+)
+
+
+class Command(CollectStaticCommand):
+    def handle(self, *args, **options):
+        call_command("tailwind", "build")
+        super().handle(*args, **options)
