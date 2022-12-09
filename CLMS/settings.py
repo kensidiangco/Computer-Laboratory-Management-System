536c58de5d76430c00220624e990586adfb206f9
kensidiangco <kensidiangco@gmail.com>
2022-12-10 02:07:11 +0800

    deploy to railway

--- CLMS/settings.py
+++ CLMS/settings.py
@@ -158,10 +158,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    BASE_DIR / 'staticfiles'
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
