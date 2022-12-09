cbed14f58d90ae8d85b355f5941a5136b2e6a31d
kensidiangco <kensidiangco@gmail.com>
2022-12-09 14:19:16 +0800

    Custom commit

--- CLMS/settings.py
+++ CLMS/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'https://clms-sjc.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 DEBUG_PROPAGATE_EXCEPTIONS = True
 
